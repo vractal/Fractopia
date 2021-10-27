7c88ba00a55940c7cc74569d31a7c2f24f467129
vractal <v@fractopia.org>
2021-10-27 02:05:24 -0300

    Add url for gitlab pages

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,20 @@
-const publicPath = '/'
+const publicPath =
+  process.env.NODE_ENV === "production" ? "/fractopia-solid-prototype/" : "/";
 
 module.exports = {
   // options...
   publicPath: publicPath,
 
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     //http://kazupon.github.io/vue-i18n/guide/sfc.html#webpack
     config.module
-    .rule("i18n")
-    .resourceQuery(/blockType=i18n/)
-    .type('javascript/auto')
-    .use("i18n")
-    .loader("@kazupon/vue-i18n-loader")
-    .end();
+      .rule("i18n")
+      .resourceQuery(/blockType=i18n/)
+      .type("javascript/auto")
+      .use("i18n")
+      .loader("@kazupon/vue-i18n-loader")
+      .end();
   },
 
-  transpileDependencies: [
-    'vuetify'
-  ]
-}
+  transpileDependencies: ["vuetify"],
+};
